a8a48d200571d0d011544fc51afc021625537974
PPLAX2835 <lax1458667357@gmail.com>
2023-09-05 16:43:11 +0800

    FileMapper 还不知道要不要写

---
+++ src/main/java/xyz/pplax/pplaxnetdisk/mapper/FileMapper.java
@@ -0,0 +1,14 @@
+package xyz.pplax.pplaxnetdisk.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import xyz.pplax.pplaxnetdisk.domain.File;
+
+import java.util.List;
+
+public interface FileMapper extends BaseMapper<File> {
+
+
+//    void batchInsertFile(List<File> fileList);
+
+}
