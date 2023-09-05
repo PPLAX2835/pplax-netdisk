8ba3e839928e1e4b0944151c3903d3664eecfb85
PPLAX2835 <lax1458667357@gmail.com>
2023-09-05 11:18:11 +0800

    xyz.pplax.pplaxnetdist.mapper     CommonFileMapper

---
+++ src/main/java/xyz/pplax/pplaxnetdisk/mapper/CommonFileMapper.java
@@ -0,0 +1,17 @@
+package xyz.pplax.pplaxnetdisk.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import xyz.pplax.pplaxnetdisk.domain.CommonFile;
+import xyz.pplax.pplaxnetdisk.vo.commonfile.CommonFileListVo;
+import xyz.pplax.pplaxnetdisk.vo.commonfile.CommonFileUser;
+
+import java.util.List;
+
+@Mapper
+public interface CommonFileMapper extends BaseMapper<CommonFile> {
+    List<CommonFileUser> selectCommonFileUser(@Param("userId") Long userId);
+    List<CommonFileListVo> selectCommonFileByUser(@Param("userId") Long userId, @Param("sessionUserId") Long sessionUserId);
+
+}
--- src/test/java/xyz/pplax/pplaxnetdisk/PPLAXNetdiskApplicationTests.java
+++ src/test/java/xyz/pplax/pplaxnetdisk/PPLAXNetdiskApplicationTests.java
@@ -1,8 +1,14 @@
 package xyz.pplax.pplaxnetdisk;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import xyz.pplax.pplaxnetdisk.io.PPLAXFile;
+import xyz.pplax.pplaxnetdisk.mapper.CommonFileMapper;
+import xyz.pplax.pplaxnetdisk.vo.commonfile.CommonFileListVo;
+import xyz.pplax.pplaxnetdisk.vo.commonfile.CommonFileUser;
+
+import java.util.List;
 
 @SpringBootTest
 class PPLAXNetdiskApplicationTests {
@@ -16,6 +22,16 @@ class PPLAXNetdiskApplicationTests {
     void PPLAXFileTest() {
         PPLAXFile pplaxFile = new PPLAXFile("G:\\tmp", true);
         System.out.println(pplaxFile.getParent());
+    }
+
+    @Autowired
+    CommonFileMapper commonFileMapper;
+    @Test
+    void PPLAXCommonFileMapperTest() {
+        List<CommonFileUser> commonFileUsers = commonFileMapper.selectCommonFileUser(1l);
+        List<CommonFileListVo> commonFileListVos = commonFileMapper.selectCommonFileByUser(1l, 1l);
+        System.out.println(commonFileUsers.toString());
+        System.out.println(commonFileListVos.toString());
     }
 
 }
