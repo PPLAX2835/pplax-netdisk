7b7f07d00964b7069cd573f5b894678e5e565d64
PPLAX2835 <lax1458667357@gmail.com>
2023-09-05 09:59:59 +0800

    新建项目

---
+++ src/test/java/xyz/pplax/pplaxnetdisk/PPLAXNetdiskApplicationTests.java
@@ -0,0 +1,14 @@
+package xyz.pplax.pplaxnetdisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PPLAXNetdiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Hello World!");
+    }
+
+}
