3890b3573a1abcbeb6389431924cedd0adc8e5a0
PPLAX2835 <lax1458667357@gmail.com>
2023-09-06 20:05:02 +0800

    Entity

---
+++ src/main/java/xyz/pplax/pplaxnetdisk/Entity/DiskuserRepository.java
@@ -0,0 +1,8 @@
+package xyz.pplax.pplaxnetdisk.Entity;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface DiskuserRepository extends PagingAndSortingRepository<Diskuser, String> {
+
+}
