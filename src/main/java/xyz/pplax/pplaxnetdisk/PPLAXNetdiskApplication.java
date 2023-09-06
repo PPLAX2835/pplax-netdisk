d25f740f3a3e78f29c0dcb685bb796fe022815e9
PPLAX2835 <lax1458667357@gmail.com>
2023-09-06 21:13:14 +0800

    property

--- src/main/java/xyz/pplax/pplaxnetdisk/PPLAXNetdiskApplication.java
+++ src/main/java/xyz/pplax/pplaxnetdisk/PPLAXNetdiskApplication.java
@@ -2,8 +2,13 @@ package xyz.pplax.pplaxnetdisk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import xyz.pplax.pplaxnetdisk.property.StorageProperties;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableConfigurationProperties(StorageProperties.class)
 public class PPLAXNetdiskApplication {
 
     public static void main(String[] args) {
