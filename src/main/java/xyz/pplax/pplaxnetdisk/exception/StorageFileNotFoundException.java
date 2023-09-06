8af5bf4125896d2feeeff5df2abac5c798a029e8
PPLAX2835 <lax1458667357@gmail.com>
2023-09-06 21:51:45 +0800

    自定义异常

---
+++ src/main/java/xyz/pplax/pplaxnetdisk/exception/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package xyz.pplax.pplaxnetdisk.exception;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
