f1a63a977d47bac1f9653b3340d9d6b1f1effeb2
PPLAX2835 <lax1458667357@gmail.com>
2023-09-06 17:55:57 +0800

    调整包

--- src/main/java/xyz/pplax/pplaxnetdisk/PPLAXNetdiskApplication.java
+++ src/main/java/xyz/pplax/pplaxnetdisk/PPLAXNetdiskApplication.java
@@ -1,4 +1,4 @@
-package com.example.pplaxnetdisk;
+package xyz.pplax.pplaxnetdisk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/xyz/pplax/pplaxnetdisk/PPLAXNetdiskApplicationTests.java
+++ src/test/java/xyz/pplax/pplaxnetdisk/PPLAXNetdiskApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.pplaxnetdisk;
+package xyz.pplax.pplaxnetdisk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
