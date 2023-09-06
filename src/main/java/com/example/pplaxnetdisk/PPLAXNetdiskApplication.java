5be84e66bf3ec8a84104e74db229b10f89c45f2c
PPLAX2835 <lax1458667357@gmail.com>
2023-09-06 17:54:41 +0800

    重新开始了

---
+++ src/main/java/com/example/pplaxnetdisk/PPLAXNetdiskApplication.java
@@ -0,0 +1,13 @@
+package com.example.pplaxnetdisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PPLAXNetdiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PPLAXNetdiskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pplaxnetdisk/PPLAXNetdiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pplaxnetdisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PPLAXNetdiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
