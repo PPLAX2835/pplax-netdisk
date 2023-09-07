b5e99cffe48b7434829e440ab89825fa135f5038
PPLAX2835 <lax1458667357@gmail.com>
2023-09-07 15:56:15 +0800

    module.config.model module.link.model.enums.RefererTypeEnum

---
+++ src/main/java/xyz/pplax/netdisk/module/config/model/request/TestAntPathMatcherRequest.java
@@ -0,0 +1,12 @@
+package xyz.pplax.netdisk.module.config.model.request;
+
+import lombok.Data;
+
+@Data
+public class TestAntPathMatcherRequest {
+
+    private String antPath;
+
+    private String testPath;
+
+}
