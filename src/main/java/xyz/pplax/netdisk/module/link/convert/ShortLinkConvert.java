42549d388731b0fa57b07c42c517f07d596de9d2
PPLAX2835 <lax1458667357@gmail.com>
2023-09-07 19:52:16 +0800

    xyz.pplax.netdisk.modle.link.convert.ShortLinkConvert

---
+++ src/main/java/xyz/pplax/netdisk/module/link/convert/ShortLinkConvert.java
@@ -0,0 +1,24 @@
+package xyz.pplax.netdisk.module.link.convert;
+
+import xyz.pplax.netdisk.module.link.model.entity.ShortLink;
+import xyz.pplax.netdisk.module.link.model.request.ShortLinkResult;
+import xyz.pplax.netdisk.module.storage.model.entity.StorageSource;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
+
+/**
+ * 直链实体类器
+ *
+ * @author zhaojun
+ */
+@Component
+@Mapper(componentModel = "spring")
+public interface ShortLinkConvert {
+
+	@Mapping(source = "shortLink.id", target = "id")
+	@Mapping(source = "storageSource.name", target = "storageName")
+	@Mapping(source = "storageSource.type", target = "storageType")
+	ShortLinkResult entityToResultList(ShortLink shortLink, StorageSource storageSource);
+
+}
